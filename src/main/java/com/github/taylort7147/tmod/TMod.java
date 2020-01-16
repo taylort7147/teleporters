436a5b7944a8cd6744302ff87b5294eddb7832f8
Turnidge, Taylor S <taylort7147@gmail.com>
2020-01-15 21:25:44 -0800

    Added TMod class and updated metadata

---
+++ src/main/java/com/github/taylort7147/tmod/TMod.java
@@ -0,0 +1,10 @@
+package com.github.taylort7147.tmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(TMod.MODID)
+public final class TMod
+{
+    public static final String MODID = "tmod";
+}
